64b31f189c920072fe2cfd01995ec975b1222688
Raphaël Morineau <raphael.morineau@gmail.com>
2022-06-09 17:28:19 +0200

    Wrap SideMenu's nav inside an aside

--- app/components/layout/SideMenu.tsx
+++ app/components/layout/SideMenu.tsx
@@ -2,11 +2,15 @@ import { NavLink } from '@remix-run/react';
 
 export function SideMenu() {
   return (
-    <nav>
-      <NavLink to="/examples">Examples Section</NavLink>
-      <NavLink to="/toto">Tatas Section</NavLink>
-      <NavLink to="/tata">Titis Section</NavLink>
-      <NavLink to="/titi">Totos Section</NavLink>
-    </nav>
+    <aside>
+      <h2>Navigation</h2>
+
+      <nav>
+        <NavLink to="/examples">Examples Section</NavLink>
+        <NavLink to="/toto">Tatas Section</NavLink>
+        <NavLink to="/tata">Titis Section</NavLink>
+        <NavLink to="/titi">Totos Section</NavLink>
+      </nav>
+    </aside>
   );
 }
